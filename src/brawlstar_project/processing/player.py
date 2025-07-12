0af0982288434eba7fcc1e98fc27eeb79ce4e16e
Xavier DAVE <dave.xavier30@gmail.com>
2025-07-12 11:42:56 +0200

    feat: add pytest

--- src/brawlstar_project/processing/player.py
+++ src/brawlstar_project/processing/player.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-
+import re
 
 @dataclass
 class Player:
@@ -9,10 +9,16 @@ class Player:
         if not self.tag.startswith("#"):
             self.tag = "#" + self.tag
 
+        player_tag = self.tag[1:]
+
+        # Check if player tag contains 8 caracters
+        if len(player_tag) != 8:
+            raise ValueError("Player tag must be exactly 8 characters after '#'")
+
+        # Player tag should only contains letters and numbers
+        if not re.fullmatch(r"[A-Z0-9]{8}", player_tag):
+            raise ValueError("Player tag must contain only uppercase letters A-Z and digits 0-9")
+
     @property
     def formatted_tag(self) -> str:
-        """
-        Returns the tag formatted for use in the API URL
-        (replace '#' with '%23').
-        """
         return self.tag.replace("#", "%23")
